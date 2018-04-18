19c0718433b2ed6a7896b4827058fceaf556a71c
Tim <timiscoding@gmail.com>
2018-04-18 16:22:38 +1000

    Add GET /stores, GET /, GET /add, POST /add, POST /add/:id and GET /stores/:id/edit. Add geocoding autocomplete when editing a store

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,3 +1,36 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   res.render('index');
+};
+
+exports.addStore = (req, res) => {
+  res.render('editStore', {title: 'Add store'});
+};
+
+exports.createStore = async (req, res) => {
+  const store = await new Store(req.body).save();
+  req.flash('info', `Successfully created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  console.log(stores);
+  res.render('stores', {title: 'Stores', stores});
+};
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({_id: req.params.id});
+  res.render('editStore', {title: 'Edit store', store});
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  req.flash('success', `Succesfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View store</a>`);
+  res.redirect(`/stores/${store._id}/edit`);
 };
