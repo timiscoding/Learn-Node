e2e47a98e9c817c04ec1068140cd310672d2b6c9
Tim <timiscoding@gmail.com>
2018-04-24 10:47:35 +1000

    Add GET /hearts to show hearted stores

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -50,5 +50,6 @@ router.get('/map', storeController.map);
 router.get('/api/search', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
 router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore));
+router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
 
 module.exports = router;
